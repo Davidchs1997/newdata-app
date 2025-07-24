952dbdc5a8de6714886724e9164effd7e558555b
Davidchs1997 <davidchacons26@gmail.com>
2025-07-23 20:46:45 -0600

    fix: added layout to upload page

---
+++ app/upload/layout.tsx
@@ -0,0 +1,7 @@
+export default function UploadLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="p-4 max-w-5xl mx-auto">
+      {children}
+    </section>
+  );
+}
