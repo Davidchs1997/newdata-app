9545c3be30455e39059065533f281a9ce693bd49
Davidchs1997 <davidchacons26@gmail.com>
2025-07-25 14:36:02 -0600

    Fix: Add button and checkbox components + update tsconfig

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,28 +1,16 @@
-"use client";
+import React from "react";
 
-import * as React from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
-import { cn } from "@/lib/utils";
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
 
-export interface CheckboxProps extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {}
+export const Checkbox: React.FC<CheckboxProps> = ({ label, ...props }) => {
+  return (
+    <label className="flex items-center space-x-2">
+      <input type="checkbox" {...props} className="form-checkbox" />
+      <span>{label}</span>
+    </label>
+  );
+};
 
-const Checkbox = React.forwardRef<React.ElementRef<typeof CheckboxPrimitive.Root>, CheckboxProps>(
-  ({ className, ...props }, ref) => (
-    <CheckboxPrimitive.Root
-      ref={ref}
-      className={cn(
-        "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
-        className
-      )}
-      {...props}
-    >
-      <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
-        <CheckIcon className="h-4 w-4" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-  )
-);
-Checkbox.displayName = "Checkbox";
-
-export { Checkbox };
+export default Checkbox;
